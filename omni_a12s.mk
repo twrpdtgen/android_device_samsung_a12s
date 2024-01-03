a6269299179b71e3e30184f265719feccae71ce3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-03 16:16:02 +0100

    a12s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A127F Script version: 3.0.0

---
+++ omni_a12s.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a12s device
+$(call inherit-product, device/samsung/a12s/device.mk)
+
+PRODUCT_DEVICE := a12s
+PRODUCT_NAME := omni_a12s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A127F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a12snsxx-user 13 TP1A.220624.014 A127FXXUADWK1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a12snsxx/a12s:13/TP1A.220624.014/A127FXXUADWK1:user/release-keys
