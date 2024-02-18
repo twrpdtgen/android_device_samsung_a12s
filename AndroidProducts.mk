106a331f1d863654f58938bea20e7661853fefd8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-02-18 16:03:59 +0100

    a12s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A127M Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a12s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a12s-user \
+    omni_a12s-userdebug \
+    omni_a12s-eng
